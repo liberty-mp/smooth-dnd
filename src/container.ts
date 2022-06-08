0bffd562176a02d062b0cdb7782e13eb85abe059
Narcis B <leonardssh22@gmail.com>
2022-06-08 22:54:33 +0300

    add origin to handle drop

--- src/container.ts
+++ src/container.ts
@@ -163,14 +163,13 @@ function handleDrop({ element, draggables, layout, getOptions }: ContainerProps)
     // if drop zone is valid => complete drag else do nothing everything will be reverted by draggablesReset()
     if (!draggableInfo.cancelDrop) {
       if (draggableInfo.targetElement || getOptions().removeOnDropOut || forDispose) {
-        let actualAddIndex =
-          addedIndex !== null ? (removedIndex !== null && removedIndex < addedIndex ? addedIndex - 1 : addedIndex) : null;
+        let actualAddIndex = addedIndex !== null ? (removedIndex !== null && removedIndex < addedIndex ? addedIndex - 1 : addedIndex) : null;
         const dropHandlerParams = {
           removedIndex,
           addedIndex: actualAddIndex,
           payload: draggableInfo.payload,
           whereComeFrom,
-          options: getOptions()
+          origin: draggableInfo.container.getOptions().groupName,
         };
         
         dropHandler(dropHandlerParams, getOptions().onDrop);
